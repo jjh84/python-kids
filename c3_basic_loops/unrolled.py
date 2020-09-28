c09114f166800241f63c5115ae0be56d88d32515
Jeong Jihoon <jjh_t@kaist.ac.kr>
2020-09-29 08:21:56 +0900

    initial commit

---
+++ c3_basic_loops/unrolled.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+
+#
+# 동일한 문장을 10번 화면에 출력해 보아요.
+#
+
+print('안녕!')
+print('안녕!')
+print('안녕!')
+print('안녕!')
+print('안녕!')
+print('안녕!')
+print('안녕!')
+print('안녕!')
+print('안녕!')
+print('안녕!')
